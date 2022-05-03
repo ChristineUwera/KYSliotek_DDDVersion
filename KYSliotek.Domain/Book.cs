eea82d0272d3e1104994b952bc3230f2964d6da9
Christine <chri15eg@edu.ucl.dk>
2022-05-03 09:48:01 +0200

    dgfhjk

--- KYSliotek.Domain/Book.cs
+++ KYSliotek.Domain/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KYSliotek.Domain.Shared;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,22 +7,42 @@ namespace KYSliotek.Domain
 {
     public class Book
     {
-        public BookId Id { get;}
+        public BookId Id { get; private set; }
         public AuthorId AuthorId { get; private set; }
-        public string _title { get; set; }
-        public string description { get; set; }
+        public BookTitle Title { get; private set; }
+        public BookDescription Description { get; private set; }        
+        public BookStatus Status { get; private set; }
+        
         //need a picture as well
 
         public Book(BookId id, AuthorId authorId)
-        {
-            if (id == default)
-                throw new ArgumentException("Identity must be specified", nameof(id));
-            if(authorId == default)
-                throw new ArgumentException("owner id must be specified", nameof(authorId));
-
+        {          
             Id = id;
             AuthorId = authorId;
+            Status = BookStatus.Inactive;
         }
 
+        public void SetTile(BookTitle title) => Title = title;
+
+        public void UpdateDescription(BookDescription description) => Description = description;
+        
+        public void RequestToPublish()
+        {
+            if (Title == null)
+                throw new InvalidEntityStateException(this, "title cannot be empty");
+            if (Description == null)
+                throw new InvalidEntityStateException(this, "description cannot be empty");
+            
+            Status = BookStatus.PendingReview;
+        }
+
+
+        public enum BookStatus
+        {
+            PendingReview,
+            Active,
+            Inactive,
+            MarkedAsUdlånt      //bogen er udlånt
+        }
     }
 }
---
+++ KYSliotek.Domain/BookDescription.cs
@@ -0,0 +1,30 @@
+﻿using KYSliotek.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KYSliotek.Domain
+{
+    public class BookDescription : Value<BookDescription>
+    {
+        public string Value { get; private set; }
+
+        internal BookDescription(string value)
+        {
+            Value = value;
+        }
+
+        //factory method converting a string to value object
+        public static BookDescription FromString(string title)
+        {
+            return new BookDescription(title);
+        }
+
+        public static implicit operator string(BookDescription text) =>
+           text.Value;
+
+
+        // Satisfy the serialization requirements
+        protected BookDescription() { }
+    }
+}

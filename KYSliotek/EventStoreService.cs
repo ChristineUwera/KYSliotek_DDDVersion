c4eb14bd02fcbb4a54e855b05212f62fdd50266a
Christine <chri15eg@edu.ucl.dk>
2022-05-16 01:40:33 +0200

    managingSubscriptionToEvents

---
+++ KYSliotek/EventStoreService.cs
@@ -0,0 +1,38 @@
+﻿using EventStore.ClientAPI;
+using KYSliotek.Infrastructure;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KYSliotek
+{
+    //handling event store connection. Connect and Disconnect
+    public class EventStoreService : IHostedService
+    {
+        private readonly IEventStoreConnection _esConnection;
+        private readonly ProjectionManager _projectionManager;
+
+        public EventStoreService(IEventStoreConnection esconnection, ProjectionManager projectionManager)
+        {
+            _esConnection = esconnection;
+            _projectionManager = projectionManager;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _esConnection.ConnectAsync();
+            _projectionManager.Start();//start subscriprion as soon as es is connected
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _projectionManager.Stop();
+            _esConnection.Close();
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- KYSliotek/Infrastructure/ProjectionManager.cs
+++ KYSliotek/Infrastructure/ProjectionManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EventStore.ClientAPI;
+using KYSliotek.Framework;
+using Serilog;
+using Serilog.Events;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,41 @@ namespace KYSliotek.Infrastructure
 {
     public class ProjectionManager
     {
+        private readonly IEventStoreConnection _connection;
+        private readonly IProjection[] _projection;
+        private EventStoreAllCatchUpSubscription _subscription;
+
+        public ProjectionManager(IEventStoreConnection connection, params IProjection[] projection)
+        {
+            _connection = connection;
+            _projection = projection;
+        }
+
+        //to start the subscription, the connection needs to be open
+        public void Start()
+        {
+            var settings = new CatchUpSubscriptionSettings(2000, 500,
+                Log.IsEnabled(LogEventLevel.Verbose), false, "try-out-subscription");
+            //here false is for the resolveLinkTos field, and once set to true, it makes events for our aggregates be projected many times
+
+            _subscription = _connection.SubscribeToAllFrom(Position.Start, settings, EventAppeared);
+        }
+        //here we say Position.Start coz it's inMemory collection and we will read it again and again from start
+
+
+        private Task EventAppeared(EventStoreCatchUpSubscription subscription, ResolvedEvent resolvedEvent)
+        {
+            if (resolvedEvent.Event.EventType.StartsWith("$"))
+                return Task.CompletedTask;
+
+            var @event = resolvedEvent.Deserialzie();
+
+            Log.Debug("Projecting event {type}", @event.GetType().Name);
+
+            return Task.WhenAll(_projection.Select(x => x.Project(@event)));
+        }
+
+        //to stop the subscription
+        public void Stop() => _subscription.Stop();
     }
 }

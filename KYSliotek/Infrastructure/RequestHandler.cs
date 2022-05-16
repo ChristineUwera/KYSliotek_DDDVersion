9b9aab8a8bd0a9cd4b7f1d4b0cff64db10928eff
Christine <chri15eg@edu.ucl.dk>
2022-05-16 14:24:03 +0200

    fdghjk

--- KYSliotek/Infrastructure/RequestHandler.cs
+++ KYSliotek/Infrastructure/RequestHandler.cs
@@ -31,12 +31,12 @@ namespace KYSliotek.Infrastructure
         }
 
         //we will use this extension method for all queries
-        public static async Task<IActionResult> HandleQuery<TModel>(
-          Func<Task<TModel>> query, ILogger log)
+        public static IActionResult HandleQuery<TModel>(
+          Func<TModel> query, ILogger log)
         {
             try
             {
-                return new OkObjectResult(await query());
+                return new OkObjectResult(query());
             }
             catch (Exception e)
             {
